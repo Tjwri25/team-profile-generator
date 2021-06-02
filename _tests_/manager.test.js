78e6d2a57c2df5df03ce06fc6969273ab4651cd2
Taylor Wright <tjwri25@gmail.com>
2021-06-02 01:03:43 -0400

    tests 100%

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,16 +1,16 @@
 const Manager = require('../lib/Manager.js');
 
 test('creates a manager object', () => {
-    const manager = new Manager('John Smith', '123123', 'john@john.com', '123');
+    const manager = new Manager('John Smith', '123123', 'john@john.com', '3');
     
     expect(manager.name).toBe('John Smith');
     expect(manager.id).toBe('123123');
     expect(manager.email).toBe('john@john.com');
-    expect(manager.office).toBe('123');
+    expect(manager.officeNumber).toBe('3');
 });
 
 test("gets employee's role", () => {
-    const manager = new Manager('John Smith', '123123', 'john@john.com', '123');
+    const manager = new Manager('John Smith', '123123', 'john@john.com', '3');
 
     expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
 });
